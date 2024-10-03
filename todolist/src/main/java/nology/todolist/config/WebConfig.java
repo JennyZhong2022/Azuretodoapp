ca7b8eeabd32f06306ff5fd40b57d52f28f52eef
Xiao Zhong <zhongxiao2017@gmail.com>
2024-10-03 23:21:09 +1000

    webconfig update

--- todolist/src/main/java/nology/todolist/config/WebConfig.java
+++ todolist/src/main/java/nology/todolist/config/WebConfig.java
@@ -6,12 +6,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+  @Override
   public void addCorsMappings(CorsRegistry registry) {
     String[] allowedOrigins = {
-        "http://localhost:5173/",
-        "http://127.0.0.1:5173/",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
         "http://jennytodo-list.s3-website-ap-southeast-2.amazonaws.com",
-        "https://black-coast-090609c00.5.azurestaticapps.net/" };
-    registry.addMapping("/**").allowedOrigins(allowedOrigins).allowedMethods("*").allowedHeaders("*");
+        "https://black-coast-090609c00.5.azurestaticapps.net"
+    };
+    registry.addMapping("/**")
+        .allowedOrigins(allowedOrigins)
+        .allowedMethods("*")
+        .allowedHeaders("*");
   }
 }
